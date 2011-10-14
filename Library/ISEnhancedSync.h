990c84cff7a7cb3742b3abfc9f0e0bd1e4f881b4
Maher Ali <maher.ali@gmail.com>
2011-10-14 18:29:19 -0500

    updated Library

--- Library/ISEnhancedSync.h
+++ Library/ISEnhancedSync.h
@@ -1,5 +1,12 @@
 #import "ISBasicSync.h"
 
+/** A Sync class with JSON over HTTP and Offline capabilities
+ 
+ To use, just return this class from the [ISModel syncClass] or [ISCollection synClass] method.
+ For a specific database name, use setDBName:andVersion: before synching. Only needs to be done once.
+ Data is caches for all requests, to partition the cached data, provide the key in SYNC_CACHE_USER_KEY 
+ in th eoptions hash of syncWithOptions: method.
+ */
 @interface ISEnhancedSync : ISBasicSync
 
 
