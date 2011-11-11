37084f697ac7d763fca7a7eac6ed7811e6578dd1
Maher Ali <alime@me.com>
2011-11-11 16:40:35 -0600

    work on HTTP Basic Auth app

--- HTTPBasicAuthApp/App/AppDelegate.m
+++ HTTPBasicAuthApp/App/AppDelegate.m
@@ -12,20 +12,26 @@
     return YES;
 }
 
+- (void) showMKT{
+    __block AppDelegate *this = self;
+    [this.marketing fetch:$dict(SUCCESS_HANDLER_KEY, $block(^(Marketing *model, NSData *data){
+        [UIAlertView message:$array(@"Success marketing!")];
+    }), FAILURE_HANDLER_KEY, $block(^(Marketing *model, NSArray *errors){
+        [UIAlertView errors:errors];
+    }))];
+}
+
 - (void)applicationDidBecomeActive:(UIApplication *)application{
     __block AppDelegate *this = self;
     [ISModel setBaseUrl:@"http://10.211.55.4:3000/"];
-    self.session = [[[Session alloc] initWithAttributes:$dict(@"user_name", @"alime@me.com", @"password", @"test123 ") andOptions:$dict()] autorelease];
+    self.session = [[[Session alloc] initWithAttributes:$dict(@"user_name", @"alime@me.com", @"password", @"test123") andOptions:$dict()] autorelease];
     self.marketing = [[[Marketing alloc] initWithAttributes:$dict() andOptions:$dict()] autorelease];
     [session save:$dict(SUCCESS_HANDLER_KEY, $block(^(Session *model, NSData *data){
         [UIAlertView message:$array(@"Success logging to server!")];
+        [this showMKT];
     }), FAILURE_HANDLER_KEY, $block(^(Session *model, NSArray *errors){
         [UIAlertView errors:errors];
-        [this.marketing fetch:$dict(SUCCESS_HANDLER_KEY, $block(^(Marketing *model, NSData *data){
-            [UIAlertView message:$array(@"Success marketing!")];
-        }), FAILURE_HANDLER_KEY, $block(^(Marketing *model, NSArray *errors){
-            [UIAlertView errors:errors];
-        }))];
+        [this showMKT];
     }))];
 }
 
