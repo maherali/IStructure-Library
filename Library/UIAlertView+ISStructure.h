5d04253cd5f8de960a8488774150e052c5f87a58
Maher Ali <alime@me.com>
2011-11-13 13:44:02 -0600

    updated Library

--- Library/UIAlertView+ISStructure.h
+++ Library/UIAlertView+ISStructure.h
@@ -1,5 +1,8 @@
 @interface UIAlertView (ISStructure)
+
 + (void) errors:(NSArray*) errors;
 + (void) allParams:(NSArray*) values;
-+ (void) message:(NSArray*) values;
++ (void) message:(NSString*) message;
++ (void) messages:(NSArray*) values;
+
 @end
