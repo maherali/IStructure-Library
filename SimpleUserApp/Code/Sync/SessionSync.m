97f7be4a22c466d5d1d18fb7cd42d5eee6ea1a02
Maher Ali <alime@me.com>
2011-11-14 20:25:32 -0600

    Explanation of signup sync.

--- SimpleUserApp/Code/Sync/SessionSync.m
+++ SimpleUserApp/Code/Sync/SessionSync.m
@@ -2,6 +2,7 @@
 
 @implementation SessionSync
 
+// To create a session (login), the client needs to send the Authorization (Basic Http authentication).
 - (NSMutableURLRequest*) lastChanceToUpdateRequest:(NSMutableURLRequest*) request{
     [request addBasicHTTPAuthHeaderForUser:[self.options objectForKey:@"USER_NAME"] withPassword:[self.options objectForKey:@"PASSWORD"]];
     return request;
