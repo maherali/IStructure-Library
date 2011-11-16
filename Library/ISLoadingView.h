a86e0832a5a376e2c7c24087bfa2d018921b14b2
Maher Ali <alime@me.com>
2011-11-15 20:57:04 -0600

    updated Library

--- Library/ISLoadingView.h
+++ Library/ISLoadingView.h
@@ -1,8 +1,12 @@
-@interface ISLoadingView : UIView
+@interface ISLoadingView : UIView{
+    id delegate;
+}
+
+@property (nonatomic, assign) id delegate;
 
 - (void)removeView;
 + (id)loadingView;
 + (id)loadingViewWithMessage:(NSString*) message;
-+ (id)loadingViewWithMessage:(NSString*) message cancellable:(BOOL) cancellable;
++ (id)loadingViewWithMessage:(NSString*) message delegate:(id) delegate;
 
 @end
